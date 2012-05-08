58b69f212b6081830916c65ade7ceab051813262
David Kerr <dave.kerr@live.ie>
2012-05-08 16:00:03 +0100

    Refactored

--- BackgroundChanger.py
+++ BackgroundChanger.py
@@ -1,69 +1,103 @@
+import urllib
 import os
 import subprocess
 import sys
-
-os.system('wget -O location.html www.ip2location.com') #download webpage with location details
+import re
 
 weather = []
-typesOfWeather = ["clear","rain","cloudy","sunny","showers"] #list of possible weather types
-backgrounds = os.getenv("HOME") + '/background/' #location of images to use for backgrounds
+typesOfWeather = ["clear","rain","cloudy","sunny","shower"]
+
+backgrounds = os.getenv("HOME") + '/.background/' #location of images to use for backgrounds
 images = "http://dl.dropbox.com/u/17715314/" #location of images online, public dropbox folder for now
+
 cel = degree_symbol = unichr(176).encode("latin-1") + "C"
-far = degree_symbol = unichr(176).encode("latin-1") + "F"
+far = degree_symbol = unichr(176).encode("latin-1") + "F"#only way to get the degree symbol to print
 
-if not os.path.exists(backgrounds): #check if directory for backgrounds exist
-    os.makedirs(backgrounds)		#if it doesnt create it
-    
-for types in typesOfWeather: #checks if the backgrounds location has all the images
-	if not os.path.exists(backgrounds + types + '.jpg'):
-		os.system('wget -O ' + backgrounds + types + '.jpg ' + images + types + '.jpg') #if not download them
+def download(location,url): #function to download webpage
+	webFile = urllib.urlopen(url)
+	localFile = open(location, 'w')
+	localFile.write(webFile.read())
+	webFile.close()
+	localFile.close()
+	
+def checkImages(): #checks if the backgrounds location has all the images
+	for types in typesOfWeather:
+		if not os.path.exists(backgrounds + types + '.jpg'):
+			download(backgrounds + types + '.jpg',images + types + '.jpg')#if not download them
 
-for line in open("location.html"):
- if "for=\"chkWeather\"" in line: #looks for weather station section 
-   weather.append(line) #and grabs the line its on
+def getLocationData():#downloads the webpage and parses the weather station out
+	download('locationData','http://www.ip2location.com')
+	
+	for line in open("locationData"):
+		if "for=\"chkWeather\"" in line: 
+			weather.append(line) 
 
-location = weather[1].split('>') 
-location = location[2].split('<')
+	location = weather[1].split('>') 
+	location = location[2].split('<')
 
-result = location[0].split('(')
-result1 = result[1].split(')')
-final = result[0] + result1[0]
-final = final.split(' ') #parses the location out of the HTML
- 
-os.system('wget -O weather.html http://www.weather.com/weather/today/' + final[0] + '+' +final[1]) #download the weather page for the correct location
+	result = location[0].split('(')
+	result1 = result[1].split(')')
 
-weather = []
-for line in open("weather.html"):
-	if "itemprop=\"weather-phrase\"" in line:#looks for weather status now section
-		weather.append(line) #and grabs the line its on
+	final = result[0] + result1[0]
+	final = final.split(' ') #parses the location out of the HTML
+	
+	os.remove('locationData')
+	return final
 
-temp = []		
-for line in open("weather.html"):
-	if "itemprop=\"temperature-fahrenheit\">" in line: #looks for the current temp
-		temp.append(line) #and grabs the line its on		
-
-output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
-output = output.split('\n') #adds a trailing newline for some reason
+def getWeatherData(location): #gets the weather.com page and returns it
+	page = urllib.urlopen("http://www.weather.com/weather/today/" + location[0] +"+" + location[1])
+	return page.read()	
+	
+def getWeather(page):#parses the weather status fr
+	weather = []
+	weather=re.compile('<span class="wx-value" itemprop="weather-phrase">(.*?)</span>').search(page)
+	currentWeather =  weather.group(1)
+	return currentWeather.lower() #gets the current weather from the page
+	
+def getTemp(page):	
+	temp = []
+	temp=re.compile('<span class="wx-value" itemprop="temperature-fahrenheit">(.*?)</span>').search(page)
+	return temp.group(1) #gets the current temp from the page
+	
+def getScreenSize():	
+	output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+	output = output.rstrip('\n') ##previous commands returns a trailing \n so rstrip removes it
+	return output
 		
-currentWeather = weather[0].split('>')
-currentWeather = currentWeather[1].split('<') #parses the weather status out e.g cloudy etc.
-currentWeather = currentWeather[0].lower() #converts it to lowercase easier to compare
+def convertToCel(currentTemp):
+	currentTemp = ((int(currentTemp) - 32) * 5/9)
+	currentTemp = str(currentTemp)
+	return currentTemp
 
-currentTemp = temp[0].split('>') #parses out the temp from the HTML
-currentTemp = currentTemp[1].split('<') #currentTemp[0] holds the tempature in Fahrenheit
+def setBackground(currentTemp,currentWeather,output):
+	for i in range(0,len(typesOfWeather)): #checks what kind of weather it is
+		if typesOfWeather[i] in currentWeather: #picks what background to use and add weather status and tempature text to it
+			if(len(sys.argv) > 1):
+				if(sys.argv[1] == 'c'):
+					os.system('convert ' + backgrounds + typesOfWeather[i] + '.jpg -resize ' + output + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,70 \'' +currentWeather + '\'\" ' + backgrounds + 'now.jpg')
+					os.system('convert ' + backgrounds + 'now.jpg -resize ' + output + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,120 \'' +currentTemp + cel + '\'\" ' + backgrounds + 'now.jpg')
+					break
+			else:#convert is a linux tool to add text to images and resize them to match the screen size
+				os.system('convert ' + backgrounds + typesOfWeather[i] + '.jpg -resize ' + output + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,70 \'' +currentWeather + '\'\" ' + backgrounds + 'now.jpg')	
+				os.system('convert ' + backgrounds + 'now.jpg -resize ' + output + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,120 \'' +currentTemp + far + '\'\" ' + backgrounds + 'now.jpg')
+				break
+	os.system('gsettings set org.gnome.desktop.background picture-uri "file:///' + backgrounds + 'now.jpg"') #sets background only works in gnome		
 
-if(len(sys.argv) > 1):
-	if(sys.argv[1] == "c"):
-		currentTemp[0] = ((int(currentTemp[0])  -  32) * 5/9)
-		currentTemp[0] = str(currentTemp[0])
+def main():
 
-for i in range(0,len(typesOfWeather)): #checks what kind of weather it is
-	if typesOfWeather[i] in currentWeather: #picks what background to use and add weather status and tempature text to it
-		if(len(sys.argv) > 1):
-			if(sys.argv[1] == 'c'):
-				os.system('convert ' + backgrounds + typesOfWeather[i] + '.jpg -resize ' + output[0] + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,70 \'' +currentWeather + ' ' + currentTemp[0] + cel + '\'\" ' + backgrounds + 'now.jpg')
-			else:
-				os.system('convert ' + backgrounds + typesOfWeather[i] + '.jpg -resize ' + output[0] + '! -font Bookman-DemiItalic -pointsize 48 -stroke White -draw \"text 25,70 \'' +currentWeather + ' ' + currentTemp[0] + far + '\'\" ' + backgrounds + 'now.jpg')	
-		os.system('gsettings set org.gnome.desktop.background picture-uri "file:///' + backgrounds + 'now.jpg"') #sets background only works in gnome
-
-os.system('rm location.html weather.html') #deletes the html files
+	if not os.path.exists(backgrounds): #check if directory for backgrounds exist
+		os.makedirs(backgrounds)#if it doesnt create it
+	
+	checkImages()	
+	location = getLocationData()
+	page = getWeatherData(location)
+	currentWeather = getWeather(page)
+	currentTemp = getTemp(page)
+	if(len(sys.argv) > 1):
+		if(sys.argv[1] == "c"):
+			currentTemp = convertToCel(currentTemp)
+	output = getScreenSize()
+	setBackground(currentTemp,currentWeather,output)
+	
+if __name__ == "__main__":
+    main()
